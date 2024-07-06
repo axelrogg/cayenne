d88aeae7038323af10feea4703ee2d920ebd0f24
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-07-06 21:57:15 +0200

    clean up matrix print function. now it looks more decent

--- src/matrix.c
+++ src/matrix.c
@@ -1,130 +1,105 @@
-#include <string.h>
-#include <stdio.h>
+#include "matrix.h"
 
-int handle_snprintf(int snprintf_returned, size_t buff_size, size_t offset) {
-    if (snprintf_returned < 0) {
-        printf("encoding error\n");
+int to_buff(char *buff, size_t buff_size, int offset, const char *format, ...) {
+    if (offset > 0 && (size_t)offset >= buff_size) {
         return -1;
-    // `snprintf_returned` is already greater than or equal to zero, so casting
+    }
+
+    va_list args;
+    va_start(args, format);
+    int num_chars = vsnprintf(buff + offset, buff_size - offset, format, args);
+    va_end(args);
+
+    if (num_chars < 0) {
+        printf("to_buff :: An output error was encountered.\n");
+        return -1;
+    // `num_chars` is already greater than or equal to zero, so casting
     // it to a `size_t` is not a problem.
-    } else if ((size_t)snprintf_returned >= buff_size - offset) { 
-        printf("output was truncated\n");
+    } else if ((size_t)num_chars >= buff_size - offset) { 
+        printf("to_buff :: Output was truncated.\n");
         return -1;
     }
-    return 0;
+    return num_chars;
 }
 
-int add_line_break(char *buff, size_t buff_size, int offset, int num_blocks) {
-    // We add 1 to `num_blocks` because we want more space for the far-left column
-    // (where the row indexes go).
-    for (int i = 0; i < num_blocks + 1; i++) {
-        int num_chars;
-        if (i == 0) {
-            num_chars = snprintf(buff + offset, buff_size - offset, "----");
-        } else {
-            num_chars = snprintf(buff + offset, buff_size - offset, "+----");
-        }
-        if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-            return -1;
-        }
-        offset += num_chars;
-    }
-
-    // Add the newline character
-    int num_chars = snprintf(buff + offset, buff_size - offset, "\n");
-    offset += num_chars;
-    if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-        return -1;
-    }
-    return offset;
-}
-
-
-int add_first_row(char *buff, size_t buff_size, int offset, int num_blocks) {
+int append_line_break(char *buff, size_t buff_size, int offset, int num_blocks) {
     int num_chars;
-    num_chars = snprintf(buff + offset, buff_size - offset, "    ");
-    if (handle_snprintf(num_chars, buff_size, offset) == -1) {
+    int new_offset = 0;
+    num_chars = to_buff(buff, buff_size, offset, "----");
+    if (num_chars == -1) {
         return -1;
-    } 
-    offset += num_chars;
+    }
+    new_offset += num_chars;
     
-    // We add 1 to `num_blocks` because we want more space for the far-left column
-    // (where the row indexes go).
     for (int i = 0; i < num_blocks; i++) {
-        if (i < 10) {
-            num_chars = snprintf(buff + offset, buff_size - offset, "|  %d ", i);
-        }
-        else {
-            num_chars = snprintf(buff + offset, buff_size - offset, "| %d ", i);
-        }
-        if (handle_snprintf(num_chars, buff_size, offset) == -1) {
+        num_chars = to_buff(buff, buff_size, new_offset + offset, "+----");
+        if (num_chars == -1) {
             return -1;
-        }    
-        offset += num_chars;
+        }
+        new_offset += num_chars;
     }
 
     // Add the newline character
-    num_chars = snprintf(buff + offset, buff_size - offset, "\n");
-    offset += num_chars;
-    if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-        return -1;
-    }
-    return offset;
-}
-
-int add_values_row(char *buff, size_t buff_size, int offset, int **matrix, int num_rows, int num_cols) {
-    int num_chars;
-    for (int i = 0; i < num_rows; i++) {
-    num_chars = snprintf(buff + offset, buff_size - offset, "  %d ", i);
-    if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-        printf("something weird happened\n");
-        return -1;
-    }
-    offset += num_chars;
-
-        for (int j = 0; j < num_cols; j++) {
-            num_chars = 0;
-            if (matrix[i][j] >= 0) {
-                num_chars = snprintf(buff + offset, buff_size - offset, "|  %d ", matrix[i][j]);
-            }
-            else {
-                num_chars = snprintf(buff + offset, buff_size - offset, "| %d ", matrix[i][j]);
-            }
-            if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-                printf("something weird happened\n");
-                return -1;
-            }
-            offset += num_chars;
-        }
-        num_chars = snprintf(buff + offset, buff_size - offset, "\n");
-        offset += num_chars;
-        if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-            printf("something weird happened\n");
-            return -1;
-        }
-        if (i < num_rows - 1) {
-            offset = add_line_break(buff, 1000, offset, num_cols);
-        }
-    }
-
-    // Add the newline character
-    num_chars = 0;
-    num_chars = snprintf(buff + offset, buff_size - offset, "\n");
-    offset += num_chars;
-    if (handle_snprintf(num_chars, buff_size, offset) == -1) {
-        printf("something weird happened\n");
-        return -1;
-    }
-    buff[offset] = '\0';
-    return offset;
+    buff[new_offset++ + offset] = '\n';
+    return new_offset;
 }
 
 void matrix_print(int **matrix, int num_rows, int num_cols) {
-    const int BUFF_SIZE = 500;
-    char buff[BUFF_SIZE];
+    char buff[MAX_BUFFER_SIZE];
     int offset = 0;
-    offset = add_first_row(buff, BUFF_SIZE, offset, num_cols);
-    offset = add_line_break(buff, BUFF_SIZE, offset, num_cols);
-    offset = add_values_row(buff, BUFF_SIZE, offset, matrix, num_rows, num_cols);
-    printf("Matrix values\n-------------\n%s", buff);
+    int num_chars;
+
+    num_chars = to_buff(buff, MAX_BUFFER_SIZE, offset, "    ");
+    if (num_chars == -1) {
+        return;
+    }
+    offset += num_chars;
+    
+    for (int i = 0; i < num_cols; i++) {
+        if (i < 10) {
+            num_chars = to_buff(buff, MAX_BUFFER_SIZE, offset, "|  %d ", i);
+        }
+        else {
+            num_chars = to_buff(buff, MAX_BUFFER_SIZE, offset, "| %d ", i);
+        }
+        if (num_chars == -1) {
+            return;
+        }
+        offset += num_chars;
+    }
+
+    // Add the newline character
+    buff[offset++] = '\n';
+    offset += append_line_break(buff, 1000, offset, num_cols);
+
+    for (int i = 0; i < num_rows; i++) {
+        num_chars = to_buff(buff, MAX_BUFFER_SIZE, offset, "  %d ", i);
+        if (num_chars == -1) {
+            return;
+        }
+        offset += num_chars;
+
+        for (int j = 0; j < num_cols; j++) {
+            if (matrix[i][j] >= 0) {
+                num_chars = to_buff(buff, MAX_BUFFER_SIZE, offset, "|  %d ", matrix[i][j]);
+            }
+            else {
+                num_chars = to_buff(buff, MAX_BUFFER_SIZE, offset, "| %d ", matrix[i][j]);
+            }
+            if (num_chars == -1) {
+                return;
+            }
+            offset += num_chars;
+        }
+        buff[offset++] = '\n';
+        if (i < num_rows - 1) {
+            offset += append_line_break(buff, 1000, offset, num_cols);
+        }
+    }
+
+    // Add the newline character
+    buff[offset++] = '\n';
+    buff[offset] = '\0';
+
+    printf("%s", buff);
 }
--- src/matrix.h
+++ src/matrix.h
@@ -1,6 +1,12 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_BUFFER_SIZE 500
+
 void matrix_print(int **matrix, int num_rows, int num_cols);
 
 #endif // !MATRIX_H
