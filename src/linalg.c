f83db284f06c3611b42a81f5dfb80982549aa816
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-07-28 23:01:56 +0200

    add linalg.c tests

--- src/linalg.c
+++ src/linalg.c
@@ -1,50 +1,15 @@
+#include <math.h>
 #include <stdlib.h>
 #include "matrix.h"
 
 
-/**
- * @brief Performs matrix multiplication and stores the result into another matrix.
- *
- * This function multiplies `matrix_a` (`rows_a` x `cols_a`) by
- * `matrix_b` (`rows_b` x `cols_b`) and stores the result into `matrix_result`.
- * To be able to multiply `matrix_a` by `matrix_b` it is necessary that `cols_a`
- * and `rows_b` are of the same value. Otherwise, the function exists returning
- * `-1`.
- *
- * We use a very naive implementation of matrix multiplication.
- *
- * @param matrix_a:      The first input matrix of dimension (`rows_a` x `cols_a`).
- * @param matrix_b:      The second input matrix of dimension (`rows_b` x `cols_b`).
- * @param matrix_result: This is where the result of the multiplication is stored.
- *                       Note that `matrix_result` does not need to be initialized
- *                       with `mat_init` before being passed into this function.
- * @param rows_a:        The number of rows in `matrix_a`.
- * @param cols_a:        The number of columns in `matrix_a`.
- * @param rows_b:        The number of rows in `matrix_b`. Must equal `cols_a`.
- * @param cols_b:        The number of columns in `matrix_b`.
- *
- * @return: Returns `0` on success and `-1` if `cols_a` is different than `rows_b`,
- *          or if memory allocation failed at some point.
- *
- * @note: The caller must ensure that `matrix_result` has enough space allocated
- *        for `rows_a` rows of pointers, and each of these pointers must be capable
- *        of pointing to an array of `cols_b` floats. This means that before
- *        `matrix_result` is passed into `matmul`, it is expected that the caller
- *        does:
- *
- *        float **matrix_result = malloc(rows_a * sizeof(float*));
- *
- *        This function takes care of the memory allocation of each row of
- *        `matrix_result`.
- */
-int matmul(float **matrix_a,
-           float **matrix_b,
-           float **matrix_result,
-           int   rows_a,
-           int   cols_a,
-           int   rows_b,
-           int   cols_b) {
-
+int linalg_matmul(double **matrix_a,
+                  double **matrix_b,
+                  double **matrix_result,
+                  int      rows_a,
+                  int      cols_a,
+                  int      rows_b,
+                  int      cols_b) {
     if (cols_a != rows_b) {
         printf("linalg.c :: matmul :: number of columns of matrix A do not match the number of rows of matrix B\n");
         return -1;
@@ -59,7 +24,7 @@ int matmul(float **matrix_a,
             return -1;
         }
          for (int j = 0; j < cols_b; j++) {
-            float sum = 0;
+            double sum = 0;
             for (int k = 0; k < cols_b; k++) {
                 sum += matrix_a[i][k] * matrix_b[k][j];
             }
@@ -70,117 +35,154 @@ int matmul(float **matrix_a,
 }
 
 
-/**
- * @brief Solves a system of linear equations of the form Ax = b.
- *
- * This function takes a square `matrix` (`n` x `n`) and an array `b` and stores
- * the result of the system into the array `x`. To solve the system, LU
- * factorization is used, relying on Crout's algorithm for the actual factorization
- * process. After that, foward and backward substitution are performed to finally
- * find the values of `x`.
- *
- * @param matrix: The input square coefficient matrix of size `n`.
- * @param n:      The dimension of `matrix`. Must be greater than 0 or the function
- *                returns -1.
- * @param b:      The array of constants on the right side of the equation Ax = b.
- * @param x:      The array where the solution of the linear system will be stored.
- *
- * @return: Returns 0 on success, or -1 on failure.
- *
- * @note: The caller must ensure that the arrays `b` and `x` have enough space
- *        allocated for `n` floats.
- */
-int lineq_solve(float** matrix, int n, float *b, float *x) {
-    float **LUI = malloc(n * sizeof(float*));
-    if (LUI == NULL) {
-        printf("linalg.c :: lineq_solve :: could not allocate memory to create matrix LUI.\n");
+int linalg_luf(double **matrix, int n, double **matrix_lu) {
+    if (matrix == NULL) {
+        printf("linalg.c :: lufactor :: matrix is NULL.\n");
         return -1;
     }
 
-    if (mat_init(LUI, n, n) == -1) {
-        mat_free(LUI, n);
-        printf("linalg.c :: lineq_solve :: matrix LUI wasn't initialized.\n");
+    if (n <= 0) {
+        printf("linalg.c :: lufactor :: size n of matrix cannot be less than or equal to 0.\n");
         return -1;
     }
+
+    if (matrix_lu == NULL) {
+        printf("linalg.c :: lufactor :: could not allocate memory to create matrix LUI.\n");
+        return -1;
+    }
+
+    // Perform LU factorization of `matrix` into `matrix_lu`. Notice that
+    // `matrix_lu` contains information about L and U. In fact,
+    // `matrix_lu` = L + U - I, where I is the n x n identity matrix.
+    for (int k = 0; k < n; k++) {
+        // This sets the values of matrix L into `matrix_lu`.
+        // We move downward within a given column. Remember that L is a lower
+        // triangular, and the values of the elements in its diagonal are not
+        // necessarily 1s.
+        for (int i = k; i < n; i++) {
+            double sum = 0;
+            for (int t = 0; t < k; t++) {
+                sum += matrix_lu[i][t] * matrix_lu[t][k];
+            }
+            matrix_lu[i][k] = matrix[i][k] - sum;
+        }
+        // This sets the values of matrix U into `matrix_lu` (with a modification).
+        // The modification is as follows. In Crout's algorithm, we establish
+        // that the elements of the diagonal of U are all 1s. However, since
+        // we want all the information of L and U to be in a single matrix LUI,
+        // we cannot satisfy that requirement (the diagonal of `matrix_lu` at this
+        // point is filled with values that belong to L). Instead, we don't
+        // bother assigning values to the diagonal of U (we already know what
+        // they are--they are all 1s).
+        // This is what makes `matrix_lu` = L + U - I.
+        for (int j = k + 1; j < n; j++) {
+            double sum = 0;
+            for (int t = 0; t < k; t++) {
+                sum += matrix_lu[k][t] * matrix_lu[t][j];
+            }
+            matrix_lu[k][j] = (matrix[k][j] - sum) / matrix_lu[k][k];
+        }
+    }
+    return 0;
+}
+
+
+double linalg_det(double **matrix, int n) {
+    if (matrix == NULL) {
+        printf("linalg.c :: linalg_det :: matrix is NULL.\n");
+        return NAN;
+    }
+    double **matrix_lu = malloc(n * sizeof(double*));
+    if (matrix_lu == NULL) {
+        printf("linalg.c :: linalg_det :: matrix_lu is NULL.\n");
+        return NAN;
+    }
+
+    if (mat_init_zeroes(matrix_lu, n, n) == -1) {
+        mat_free(matrix_lu, n);
+        printf("linalg.c :: linalg_det :: matrix_lu wasn't initialized.\n");
+        return NAN;
+    }
+
+    if (linalg_luf(matrix, n, matrix_lu) == -1) {
+        mat_free(matrix_lu, n);
+        printf("linalg.c :: linal_det :: LU factorization failed.\n");
+        return NAN;
+    }
+
+    double det = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                det *= matrix_lu[i][j];
+            }
+        }
+    }
+    mat_free(matrix_lu, n);
+    return det;
+}
+
+
+int linalg_lineq_solve(double **matrix, int n, double *b, double *x) {
+    double **matrix_lu;
 
     if (matrix == NULL) {
-        mat_free(LUI, n);
         printf("linalg.c :: lineq_solve :: matrix is NULL.\n");
         return -1;
     }
 
     if (n <= 0) {
-        mat_free(LUI, n);
         printf("linalg.c :: lineq_solve :: size n of matrix cannot be less than or equal to 0.\n");
         return -1;
     }
 
-    // Perform LU factorization of `matrix` into `LUI`. Notice that `LUI` contains
-    // information about L and U. In fact, `LUI` = L + U - I, where I is the nxn
-    // identity matrix.
-    // This is a naive implementation of Crout's algorithm.
-    for (int k = 0; k < n; k++) {
-        // This sets the values of matrix L into `LUI`.
-        // We move downward within a given column. Remember that L is a lower
-        // triangular, and the values of the elements in its diagonal are not
-        // necessarily 1s.
-        for (int i = k; i < n; i++) {
-            float sum = 0;
-            for (int t = 0; t < k; t++) {
-                sum += LUI[i][t] * LUI[t][k];
-            }
-            LUI[i][k] = matrix[i][k] - sum;
-        }
-        // This sets the values of matrix U into `LUI` (with a modification).
-        // The modification is as follows. In Crout's algorithm, we establish
-        // that the elements of the diagonal of U are all 1s. However, since
-        // we want all the information of L and U to be in a single matrix LUI,
-        // we cannot satisfy that requirement (the diagonal of `LUI` at this
-        // point is filled with values that belong to L). Instead, we don't
-        // bother assigning values to the diagonal of U (we already know what
-        // they are--they are all 1s).
-        // This is what makes `LUI` = L + U - I.
-        for (int j = k + 1; j < n; j++) {
-            float sum = 0;
-            for (int t = 0; t < k; t++) {
-                sum += LUI[k][t] * LUI[t][j];
-            }
-            LUI[k][j] = (matrix[k][j] - sum) / LUI[k][k];
-        }
+    matrix_lu = malloc(n * sizeof(double *));
+    if (matrix_lu == NULL) {
+        printf("linalg.c :: lineq_solve :: could not allocate memory to create matrix LUI.\n");
+        return -1;
+    }
+
+    if (mat_init_zeroes(matrix_lu, n, n) == -1) {
+        mat_free(matrix_lu, n);
+        printf("linalg.c :: lineq_solve :: matrix LUI wasn't initialized.\n");
+        return -1;
+    }
+
+    if (linalg_luf(matrix, n, matrix_lu) == -1) {
+        mat_free(matrix_lu, n);
+        printf("linalg.c :: linalg_lineq_solve :: LU factorization failed.\n");
     }
     
     // Perform forward substitution.
     // We solve the equation Ly = b, where `b` is the argument passed into the
     // function.
-    float *y = malloc(n * sizeof(float));
+    double *y = malloc(n * sizeof(double));
     if (y == NULL) {
-        mat_free(LUI, n);
-        printf(
-            "linalg.c :: lineq_solve :: could not allocate memory to create float array y.\n"
-        );
+        mat_free(matrix_lu, n);
+        printf("linalg.c :: linalg_lineq_solve :: could not allocate memory to create float array y.\n");
         return -1;
     }
-    for (int i = 0; i < n; i++) {
-        float sum = 0;
-        for (int k = 0; k < i+1; k++) {
-            sum += matrix[i][k] * y[k];
+    for (int j = 0; j < n; j++) {
+        double sum = 0;
+        for (int k = 0; k < j + 1; k++) {
+            sum += matrix[j][k] * y[k];
         }
-        y[i] = (b[i] - sum) / matrix[i][i];
+        y[j] = (b[j] - sum) / matrix[j][j];
     }
 
     // Perform backward substitution.
     // We solve the equation Ux = y, where `y` is the vector found in the
     // forward substitution process above.
-    for (int i = n - 1; i > n; i--) {
-        float sum = 0;
-        for (int k = i + 1; k < n; k++) {
-            sum += matrix[i][k] * x[k];
+    for (int j = n - 1; j > n; j--) {
+        double sum = 0;
+        for (int k = j + 1; k < n; k++) {
+            sum += matrix[j][k] * x[k];
         }
         // No division needed here because Crout's algorithm assumes that the
         // matrix U from `LUI` has 1s on its diagonal.
-        x[i] = y[i] - sum;
+        x[j] = y[j] - sum;
     }
-    mat_free(LUI, n);
+    mat_free(matrix_lu, n);
     free(y);
     return 0;
 }
--- src/linalg.h
+++ src/linalg.h
@@ -1,65 +1,19 @@
 #ifndef LINALG_H
 
 
-/**
- * @brief Performs matrix multiplication and stores the result into another matrix.
- *
- * This function multiplies `matrix_a` (`rows_a` x `cols_a`) by
- * `matrix_b` (`rows_b` x `cols_b`) and stores the result into `matrix_result`.
- * To be able to multiply `matrix_a` by `matrix_b` it is necessary that `cols_a`
- * and `rows_b` are of the same value. Otherwise, the function exists returning
- * `-1`.
- *
- * @param matrix_a:      The first input matrix of dimension (`rows_a` x `cols_a`).
- * @param matrix_b:      The second input matrix of dimension (`rows_b` x `cols_b`).
- * @param matrix_result: This is where the result of the multiplication is stored.
- *                       Note that `matrix_result` does not need to be initialized
- *                       with `mat_init` before being passed into this function.
- * @param rows_a:        The number of rows in `matrix_a`.
- * @param cols_a:        The number of columns in `matrix_a`.
- * @param rows_b:        The number of rows in `matrix_b`. Must equal `cols_a`.
- * @param cols_b:        The number of columns in `matrix_b`.
- *
- * @return: Returns `0` on success and `-1` if `cols_a` is different than `rows_b`,
- *          or if memory allocation failed at some point.
- *
- * @note: The caller must ensure that `matrix_result` has enough space allocated
- *        for `rows_a` rows of pointers, and each of these pointers must be capable
- *        of pointing to an array of `cols_b` floats. This means that before
- *        `matrix_result` is passed into `matmul`, it is expected that the caller
- *        does:
- *
- *        float **matrix_result = malloc(rows_a * sizeof(float*));
- *
- *        This function takes care of the memory allocation of each row of
- *        `matrix_result`.
- */
-int matmul(float **matrix_a,
-           float **matrix_b,
-           float **matrix_result,
-           int   rows_a,
-           int   cols_a,
-           int   rows_b,
-           int   cols_b);
+int linalg_matmul(double **matrix_a,
+           double **matrix_b,
+           double **matrix_result,
+           int      rows_a,
+           int      cols_a,
+           int      rows_b,
+           int      cols_b);
 
+int linalg_luf(double **matrix, int n, double **matrix_lu);
 
-/**
- * @brief Solves a system of linear equations of the form Ax = b.
- *
- * This function takes a square `matrix` (`n` x `n`) and an array `b` and stores
- * the result of the system into the array `x`.
- *
- * @param matrix: The input square coefficient matrix of size `n`.
- * @param n:      The dimension of `matrix`. Must be greater than 0 or the function
- *                returns -1.
- * @param b:      The array of constants on the right side of the equation Ax = b.
- * @param x:      The array where the solution of the linear system will be stored.
- *
- * @return: Returns 0 on success, or -1 on failure.
- *
- * @note: The caller must ensure that the arrays `b` and `x` have enough space
- *        allocated for `n` floats.
- */
-int lineq_solve(float** matrix, int n, float *b, float *x);
+double linalg_det(double **matrix, int n);
+
+int linalg_lineq_solve(double **matrix, int n, double *b, double *x);
+
 
 #endif // !LINALG_H
